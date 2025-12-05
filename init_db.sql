1e334bc339a138e8e778fe27d06bf5d2991d4533
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2025-12-05 23:52:08 +0300

    feat: add one line and correct query

--- init_db.sql
+++ init_db.sql
@@ -1,11 +1,12 @@
 create database product_management_db; --création db
 create user product_manager_user with password '123456'; --création user
 --permission création tables sur db
-grant create on product_management_db to product_manager_user; 
+grant create on database product_management_db to product_manager_user;
+\c product_management_db --entrer dans le db
 --permission crud sur les tables --> exixtantes
 grant insert, select, update, delete on all tables 
     in schema public to product_manager_user;
 --changer permission par défaut des tables  --> crud
 alter default privileges in schema public
-    grant insert, select, update, delete on all tables 
+    grant insert, select, update, delete on tables 
     in schema public to product_manager_user;
