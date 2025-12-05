161c60a677adfcf2c167da52088187968df9b678
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2025-12-05 23:19:28 +0300

    add init_db.sql (q1)

---
+++ init_db.sql
@@ -0,0 +1,11 @@
+create database product_management_db; --création db
+create user product_manager_user with password '123456'; --création user
+--permission création tables sur db
+grant create on product_management_db to product_manager_user; 
+--permission crud sur les tables --> exixtantes
+grant insert, select, update, delete on all tables 
+    in schema public to product_manager_user;
+--changer permission par défaut des tables  --> crud
+alter default privileges in schema public
+    grant insert, select, update, delete on all tables 
+    in schema public to product_manager_user;
